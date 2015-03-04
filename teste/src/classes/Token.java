e24a7478a334d3fb84a0182f9d80dbe25584e001
manomonxx <fernando.filipereis@gmail.com>
2015-03-04 14:01:23 -0300

    Commit Teste.

---
+++ teste/src/classes/Token.java
@@ -0,0 +1,35 @@
+package classes;
+
+
+public class Token
+{
+   public enum T
+   {
+       COMMA,
+       LET,
+       ID,
+       INT,
+       // ... definir os outros tipos de símbolos léxicos
+       EOF
+   }    
+   
+   public T type;
+   public Object val;
+   
+   public Token(T type, Object val)
+   {
+      this.type = type;
+      this.val = val;
+   }
+   
+   public Token(T type)
+   {
+      this.type = type;
+   }
+
+   @Override
+   public String toString()
+   {
+      return "Token[type=" + type + ", val=" + val + "]";
+   }
+}
