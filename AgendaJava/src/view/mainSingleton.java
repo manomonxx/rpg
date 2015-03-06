2c5a23465553276cd68db24a14c26e8a451a6f58
manomonxx <fernando.filipereis@gmail.com>
2015-03-06 20:22:45 -0300

    Singleton + Banco de Dados.

--- AgendaJava/src/view/mainSingleton.java
+++ AgendaJava/src/view/mainSingleton.java
@@ -1,19 +1,63 @@
 package view;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Pessoa;
 import controller.InstanciaBanco;
+import controller.ManipuladorBancoDados;
+import controller.ManipularPessoa;
 import controller.Singleton;
+import controller.SingletonManipuladorBancoDados;
 
 public class mainSingleton {
 	public static void main(String args[]) {
+		
+		//---------------------------------------------
+		// Teste 1
+		//---------------------------------------------
 		InstanciaBanco singleton = new InstanciaBanco();
 		
 		singleton.Teste1();
 		singleton.Teste2();
+		//---------------------------------------------
 		
-		
-		//Singleton.getInstance();
+		//---------------------------------------------
+		// Teste 2
+		//---------------------------------------------
 		String Sinstance = Singleton.Teste();
 		
 		System.out.println(Sinstance);
+		//---------------------------------------------
+		
+		//---------------------------------------------
+		//Teste 3
+		//---------------------------------------------
+		try {
+			listarPessoas();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//System.out.println(SingletonManipuladorBancoDados.getInstance().selectTelefones(telefone));
+		//---------------------------------------------
+	}
+	
+	
+	private static void listarPessoas() throws SQLException {
+		List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
+		SingletonManipuladorBancoDados consultaBancoDados = SingletonManipuladorBancoDados.getInstance();
+		listaPessoa = consultaBancoDados.selectPessoa(null);
+		System.out
+				.println("______________________________________________________________________");
+		System.out.println("ID----NOME----TELEFONE");
+		for (int i = 0; i < listaPessoa.size(); i++)
+			System.out.println(listaPessoa.get(i).getId() + " - "
+					+ listaPessoa.get(i).getNome() + " - "
+					+ listaPessoa.get(i).getTelefone());
+		System.out
+				.println("______________________________________________________________________");
 	}
 }
